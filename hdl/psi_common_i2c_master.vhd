1e18e8dcae789bb65f71f8c456714c1e0b08e18a
Oliver Bruendler <oliver.bruendler@psi.ch>
2019-10-14 08:51:29 +0200

    DOC: Added comment about Vivado versions for a Vivado-bug workaround in psi_common_i2c_master

--- hdl/psi_common_i2c_master.vhd
+++ hdl/psi_common_i2c_master.vhd
@@ -140,7 +140,7 @@ architecture rtl of psi_common_i2c_master is
 	end record;
 	signal r, r_next : two_process_r;
 	attribute dont_touch : string;
-	attribute dont_touch of r : signal is "true";	-- Required to Fix Vivado Synthesis Bug!
+	attribute dont_touch of r : signal is "true";	-- Required to Fix Vivado 2018.2 Synthesis Bug! Is fixed in Vivado 2019.1 according to Xilinx.
 	
 	-- Tri-state buffer muxing
 	signal I2cScl_Input : std_logic;
