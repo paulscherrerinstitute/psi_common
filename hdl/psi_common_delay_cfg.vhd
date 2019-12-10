3030cfadb69678195c2bf638f5de23a038cac8b1
stef_b <benoit.stef@psi.ch>
2019-12-10 18:01:16 +0100

    BUGFIX: comment from Oliver taken into account, modification addresses to have delay space between them TODO: Documentation corresponding to do

--- hdl/psi_common_delay_cfg.vhd
+++ hdl/psi_common_delay_cfg.vhd
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --  Copyright (c) 2018 by Paul Scherrer Institute, Switzerland
 --  All rights reserved.
---  Authors: Benoît Stef & Oliver Bruendler
+--  Authors: Benoit Stef & Oliver Bruendler
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
@@ -23,104 +23,127 @@ use work.psi_common_math_pkg.all;
 -- Entity Declaration
 ------------------------------------------------------------------------------
 entity psi_common_delay_cfg is
-  generic(Width_g         : positive  := 16; --data vector width
-          Resource_g      : string    := "AUTO"; -- AUTO, SRL or BRAM
-          BramThreshold_g : positive  := 20; -- Number of delay taps to start using BRAM from (if Resource_g = AUTO)
-          MaxDelay_g      : positive  := 256; -- maximum delay wanted
-          RStPol_g        : std_logic := '1'; -- reset polarity
-          RstState_g      : boolean   := True; -- True = '0' is outputted after reset, '1' after reset the existing state is outputted
-          RamBehavior_g   : string    := "RBW" -- "RBW" = read-before-write, "WBR" = write-before-read
-         );
-  port(clk_i : in  std_logic;           -- system clock
-       rst_i : in  std_logic;           -- system reset
+  generic(Width_g       : positive  := 16; --data vector width
+          MaxDelay_g    : positive  := 256; -- maximum delay wanted
+          RStPol_g      : std_logic := '1'; -- reset polarity
+          RamBehavior_g : string    := "RBW" -- "RBW" = read-before-write, "WBR" = write-before-read
+         ); 
+         
+  port(clk_i : in  std_logic;-- system clock
+       rst_i : in  std_logic;-- system reset
        -- Data
        dat_i : in  std_logic_vector(Width_g - 1 downto 0); --data input
-       str_i : in  std_logic;           -- valid/strobe signal
+       str_i : in  std_logic;-- valid/strobe signal input
        -- #
-       del_i : in  std_logic_vector(log2ceil(MaxDelay_g) - 1 downto 0); --delay parameters input
+       del_i : in  std_logic_vector(log2ceil(MaxDelay_g) - 1 downto 0);--delay parameter input
        -- Out
-       dat_o : out std_logic_vector((Width_g - 1) downto 0)); -- data output
+       dat_o : out std_logic_vector((Width_g - 1) downto 0));-- data output
 end entity;
 
 ------------------------------------------------------------------------------
 -- Architecture Declaration
 ------------------------------------------------------------------------------
 architecture rtl of psi_common_delay_cfg is
-
-  signal mem_out_s       : std_logic_vector(Width_g - 1 downto 0);
-  signal rst_state_cnt_s : integer range 0 to MaxDelay_g - 1;
+  constant max_c              : unsigned(log2ceil(MaxDelay_g) - 1 downto 0)         := to_unsigned(MaxDelay_g-1, log2ceil(MaxDelay_g));
+  type srl_t is array (0 to 2) of std_logic_vector(Width_g - 1 downto 0);
+  signal srl_s                : srl_t                                               := (others => (others => '0'));
+  signal mem_out_s            : std_logic_vector(Width_g - 1 downto 0);
+  signal rd_addr_s, wr_addr_s : std_logic_vector(log2ceil(MaxDelay_g) - 1 downto 0) := (others => '0');
+  signal mem_out2_s           : std_logic_vector(Width_g - 1 downto 0);
+  ----------------------------------------------------------------------------
+  signal del_dff_s            : std_logic_vector(del_i'range);
+  signal latch_count_s        : unsigned(del_i'range)                               := (others => '0');
+  signal diff_s               : unsigned(del_i'range)                               := (others => '0');
+  signal rs_s                 : std_logic;
 
 begin
 
-  -- *** Assertions ***
-  assert Resource_g = "AUTO" or Resource_g = "SRL" or Resource_g = "BRAM" report "###ERROR###: psi_common_delay: Unknown Resource_g - " & Resource_g severity error;
-  assert Resource_g /= "BRAM" or MaxDelay_g >= 3 report "###ERROR###: psi_common_delay: MaxDelay_g >= 3 required for Resource_g=BRAM" severity error;
-  assert BramThreshold_g > 3 report "###ERROR###: psi_common_delay: BramThreshold_g must be > 3" severity error;
-
-  -- *** SRL ***
-  g_srl : if (MaxDelay_g > 1) and ((Resource_g = "SRL") or ((Resource_g = "AUTO") and (MaxDelay_g < BramThreshold_g))) generate
-    type srl_t is array (0 to MaxDelay_g - 1) of std_logic_vector(Width_g - 1 downto 0);
-    signal srl_s : srl_t := (others => (others => '0'));
+  --*** address control process ***
+  p_bram : process(clk_i)
+    --  variable count_start_v : unsigned(wr_addr_s'range);
   begin
-
-    p_srl : process(clk_i)
-    begin
-      if rising_edge(clk_i) then
-        if str_i = '1' then
-          srl_s(0)               <= dat_i;
-          srl_s(1 to srl_s'high) <= srl_s(0 to srl_s'high - 1);
-        end if;
-      end if;
-    end process;
-    mem_out_s <= srl_s(from_uslv(del_i) - 2);
-
-  end generate;
-
-  -- *** BRAM ***
-  g_bram : if (MaxDelay_g > 1) and ((Resource_g = "BRAM") or ((Resource_g = "AUTO") and (MaxDelay_g >= BramThreshold_g))) generate
-    signal rd_addr_s, wr_addr_s : std_logic_vector(log2ceil(MaxDelay_g) - 1 downto 0) := (others => '0');
-  begin
-
-    -- address control process
-    p_bram : process(clk_i)
-    begin
-      if rising_edge(clk_i) then
-        if rst_i = RStPol_g then
+    if rising_edge(clk_i) then
+      if rst_i = RStPol_g then
+        wr_addr_s     <= (others => '0');
+        rd_addr_s     <= (others => '0');
+        del_dff_s     <= (others => '0');
+        latch_count_s <= (others => '0');
+        rs_s          <= '0';
+      --       count_start_v := (others => '0');
+      elsif str_i = '1' then
+        del_dff_s <= del_i;
+        if from_uslv(del_i) < 3 then
           wr_addr_s <= (others => '0');
           rd_addr_s <= (others => '0');
-        elsif str_i = '1' then
-          -- write address
-          if unsigned(wr_addr_s) = unsigned(del_i) - 3 then
+        else
+          --*** write address mngt ***
+          if unsigned(wr_addr_s) = to_unsigned(MaxDelay_g - 1, wr_addr_s'length) then
             wr_addr_s <= (others => '0');
           else
             wr_addr_s <= std_logic_vector(unsigned(wr_addr_s) + 1);
           end if;
-          -- read address
-          if unsigned(rd_addr_s) = unsigned(del_i) - 3 then
-            rd_addr_s <= (others => '0');
+
+          --*** read address mngt *** for special past observation comment this statement
+          if (rs_s = '1' or del_dff_s < del_i) then
+            rd_addr_s <= rd_addr_s;
           else
-            rd_addr_s <= std_logic_vector(unsigned(rd_addr_s) + 1);
+            if unsigned(wr_addr_s) >= unsigned(del_i) - 3 and unsigned(wr_addr_s) < max_c then
+              rd_addr_s <= std_logic_vector(unsigned(wr_addr_s) - unsigned(del_i) + 3);
+            elsif unsigned(wr_addr_s) = max_c then
+              rd_addr_s <= std_logic_vector(max_c - unsigned(del_i) + 3);
+            elsif unsigned(rd_addr_s) = max_c then
+              rd_addr_s <= (others => '0');
+            else
+              rd_addr_s <= std_logic_vector(unsigned(rd_addr_s) + 1);
+            end if;
           end if;
+
+          --*** RS latch & counter  ***
+          if del_dff_s < del_i then
+            rs_s          <= '1';
+            diff_s        <= unsigned(del_i) - unsigned(del_dff_s);
+            latch_count_s <= (others => '0');
+          elsif latch_count_s = diff_s - 2 then
+            rs_s <= '0';
+          end if;
+
+          if rs_s = '1' then
+            latch_count_s <= latch_count_s + 1;
+          end if;
+
         end if;
       end if;
-    end process;
+    end if;
+  end process;
 
-    -- memory instantiation
-    i_bram : entity work.psi_common_sdp_ram
-      generic map(                      -- @suppress "Generic map uses default values. Missing optional actuals: IsAsync_g, RamStyle_g" 
-        Depth_g    => MaxDelay_g,
-        Width_g    => Width_g,
-        Behavior_g => RamBehavior_g)
-      port map(                         -- @suppress "Port map uses default values. Missing optional actuals: RdClk"
-        Clk    => clk_i,
-        WrAddr => wr_addr_s,
-        Wr     => str_i,
-        WrData => dat_i,
-        RdAddr => rd_addr_s,
-        Rd     => str_i,
-        RdData => mem_out_s
-      );
-  end generate;
+  --*** memory instantiation ***
+  i_bram : entity work.psi_common_sdp_ram
+    generic map( -- @suppress "Generic map uses default values. Missing optional actuals: IsAsync_g, RamStyle_g" 
+      Depth_g    => MaxDelay_g,
+      Width_g    => Width_g,
+      Behavior_g => RamBehavior_g)
+    port map( -- @suppress "Port map uses default values. Missing optional actuals: RdClk"
+      Clk    => clk_i,
+      WrAddr => wr_addr_s,
+      Wr     => str_i,
+      WrData => dat_i,
+      RdAddr => rd_addr_s,
+      Rd     => str_i,
+      RdData => mem_out2_s);
+
+  --*** case where the delay change below 3 -> using SRL on the fly ***
+  p_srl : process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      if str_i = '1' then
+        srl_s(0)               <= dat_i;
+        srl_s(1 to srl_s'high) <= srl_s(0 to srl_s'high - 1);
+      end if;
+    end if;
+  end process;
+
+  -- take the output of a SRL instead --
+  mem_out_s <= srl_s(from_uslv(del_i) - 2) when from_uslv(del_i) < 3 else mem_out2_s;
 
   -- *** Single Stage ***
   g_single : if MaxDelay_g = 1 generate
@@ -132,15 +155,9 @@ begin
   begin
     if rising_edge(clk_i) then
       if rst_i = RStPol_g then
-        dat_o           <= (others => '0');
-        rst_state_cnt_s <= 0;
+        dat_o <= (others => '0');
       elsif str_i = '1' then
-        if RstState_g = false or rst_state_cnt_s = unsigned(del_i) - 1 then
-          dat_o <= mem_out_s;
-        else
-          dat_o           <= (others => '0');
-          rst_state_cnt_s <= rst_state_cnt_s + 1;
-        end if;
+        dat_o <= mem_out_s;
       end if;
     end if;
   end process;
--- testbench/psi_common_delay_cfg_tb/psi_common_delay_cfg_tb.vhd
+++ testbench/psi_common_delay_cfg_tb/psi_common_delay_cfg_tb.vhd
@@ -18,14 +18,11 @@ use work.psi_tb_compare_pkg.all;
 use work.psi_common_math_pkg.all;
 
 entity psi_common_delay_cfg_tb is
-  generic(freq_clk_g      : integer  := 100E6; --clock frequency in Hz 
-          Width_g         : positive := 16; --data width in bits
-          Resource_g      : string   := "AUTO"; --resource ram
-          BramThreshold_g : positive := 64; -- threshold to use BRAM
-          MaxDelay_g      : positive := 100; -- max delay for the block
-          Delay_g         : positive := 10; -- initial delay     
-          RstState_g      : boolean  := True; -- reset stat output 0 or 1
-          RamBehavior_g   : string   := "RBW" --RAM behavior 
+  generic(freq_clk_g    : integer  := 100E6; --clock frequency in Hz 
+          Width_g       : positive := 16; --data width in bits
+          MaxDelay_g    : positive := 100; -- max delay for the block
+          Delay_g       : positive := 2; -- initial delay     
+          RamBehavior_g : string   := "RBW" --RAM behavior 
          );
 end entity;
 
@@ -62,38 +59,34 @@ begin
 
   --*** DUT***
   inst_dut : entity work.psi_common_delay_cfg
-    generic map(Width_g         => Width_g,
-                Resource_g      => Resource_g,
-                BramThreshold_g => BramThreshold_g,
-                MaxDelay_g      => MaxDelay_g,
-                RStPol_g        => '1',
-                RstState_g      => RstState_g,
-                RamBehavior_g   => RamBehavior_g)
+    generic map(Width_g       => Width_g,
+                MaxDelay_g    => MaxDelay_g,
+                RStPol_g      => '1',
+                RamBehavior_g => RamBehavior_g)
     port map(clk_i => clk_sti,
              rst_i => rst_sti,
              dat_i => dat_sti,
              str_i => str_sti,
              del_i => del_sti,
-             dat_o => dat_obs
-            );
+             dat_o => dat_obs);
 
   --*** stim process ***
   proc_stim : process
+    variable hold_v       : std_logic_vector(dat_obs'range) := (others => '0');
+    variable hold2_v      : std_logic_vector(dat_obs'range) := (others => '0');
+    variable diff_v       : integer                         := 0;
+    variable prev_value_v : integer                         := 0;
+    variable prev_delay_v : integer                         := 0;
   begin
     ----------------------------------------------------------------------------
     print(" *************************************************  ");
     print(" **          Paul Scherrer Institut             **  ");
     print(" **        psi_common_delay2_tb TestBench       **  ");
-
-    if MaxDelay_g > BramThreshold_g then
-      print(" **                Block RAM                    **  ");
-    else
-      print(" **               Shift Resgiter                **  ");
-    end if;
     print(" *************************************************  ");
     ----------------------------------------------------------------------------
     wait for period_c;
 
+    ----------------------------------------------------------------------------
     -- *** Vld high constantly ***
     print(">> Vld high constantly & initial delay");
     wait until rising_edge(clk_sti);
@@ -103,15 +96,16 @@ begin
     for i in 0 to Delay_g + 30 loop
       dat_sti <= std_logic_vector(to_unsigned(i, Width_g));
       if i < Delay_g + 1 then           --
-        assert unsigned(dat_obs) = 0 report "###ERROR###: Out data wrong, expected " & to_string(0) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        StdlvCompareInt(0, dat_obs, "Out data wrong", false);
       else
         -- output is latched on the  next rising edge, therefore shift by one
-        assert unsigned(dat_obs) = i - 1 - Delay_g report "###ERROR###: Out data wrong, expected " & to_string(i-1-Delay_g) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        StdlvCompareInt(i - 1 - Delay_g, dat_obs, "Out data wrong", false);
       end if;
       wait until rising_edge(clk_sti);
     end loop;
     dat_sti <= (others => '0');
 
+    ----------------------------------------------------------------------------
     --*** Vld high constantaly with a delay change ***
     print(">> Vld high constantly & delay change on the fly");
     wait for MaxDelay_g * period_c;
@@ -121,37 +115,38 @@ begin
     wait until rising_edge(clk_sti);
 
     str_sti <= '1';
-    for i in 0 to from_uslv(del_sti) + 30 loop
+    for i in 0 to from_uslv(del_sti) + 200 loop
       dat_sti <= std_logic_vector(to_unsigned(i, Width_g));
-      if i < 15 + 1 then                --
-        assert unsigned(dat_obs) = 0 report "###ERROR###: Out data wrong, expected " & to_string(0) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+      if i < from_uslv(del_sti) + 1 then --
+        StdlvCompareInt(0, dat_obs, "Out data wrong", false);
       else
-        -- output is latched on the  next rising edge, therefore shift by one
-        assert unsigned(dat_obs) = i - 1 - from_uslv(del_sti) report "###ERROR###: Out data wrong, expected " & to_string(i-1-from_uslv(del_sti)) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        -- output is latched on the next rising edge, therefore shift by one
+        StdlvCompareInt(i - 1 - from_uslv(del_sti), dat_obs, "Out data wrong", false);
       end if;
       wait until rising_edge(clk_sti);
     end loop;
 
     --re init
     wait for from_uslv(del_sti) * period_c;
-    del_sti <= to_uslv(20, del_sti'length);
+    --*** change delay from 15 to 10
+    del_sti <= to_uslv(10, del_sti'length);
     str_sti <= '0';
     wait for period_c;
     wait until rising_edge(clk_sti);
+    hold_v  := dat_obs;
 
+    ----------------------------------------------------------------------------
     -- *** Vld toggling ***
     print(">> Vld toggling and a new change of delay on the fly");
-    rst_sti <= '1';
     wait until rising_edge(clk_sti);
-    rst_sti <= '0';
-    wait until rising_edge(clk_sti);
+
     for i in 0 to from_uslv(del_sti) + 30 loop
       str_sti <= '1';
       dat_sti <= std_logic_vector(to_unsigned(i, Width_g));
       if i < from_uslv(del_sti) then
-        assert unsigned(dat_obs) = 0 report "###ERROR###: Out data wrong, expected " & to_string(0) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        StdlvCompareStdlv(hold_v, dat_obs, "Out data wrong");
       else
-        assert unsigned(dat_obs) = i - from_uslv(del_sti) report "###ERROR###: Out data wrong, expected " & to_string(i-from_uslv(del_sti)) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        StdlvCompareInt(i - from_uslv(del_sti), dat_obs, " Out data wrong");
       end if;
       wait until rising_edge(clk_sti);
       str_sti <= '0';
@@ -159,36 +154,57 @@ begin
       wait until rising_edge(clk_sti);
       wait until rising_edge(clk_sti);
     end loop;
-    str_sti <= '0';
+    prev_value_v := from_uslv(dat_sti);
 
     --re init
     wait for from_uslv(del_sti) * period_c;
-    del_sti <= to_uslv(MaxDelay_g, del_sti'length);
-    str_sti <= '0';
+    diff_v       := MaxDelay_g - 1 - from_uslv(del_sti);
+    prev_delay_v := from_uslv(del_sti);
+    del_sti      <= to_uslv(MaxDelay_g - 1, del_sti'length);
+
     wait for period_c;
     wait until rising_edge(clk_sti);
+    hold_v  := dat_obs;                 --due to RAM latency
+    hold2_v := std_logic_vector(unsigned(dat_obs) + to_unsigned(2, hold_v'length)); --due to RAM latency
 
+    ----------------------------------------------------------------------------
     -- *** Vld toggling with max delay ***
     print(">> Vld toggling and a new change of delay on the fly with max delay value");
-    rst_sti <= '1';
     wait until rising_edge(clk_sti);
-    rst_sti <= '0';
-    wait until rising_edge(clk_sti);
+
     for i in 0 to from_uslv(del_sti) + 30 loop
       str_sti <= '1';
-      dat_sti <= std_logic_vector(to_unsigned(i, Width_g));
-      if i < from_uslv(del_sti) then
-        assert unsigned(dat_obs) = 0 report "###ERROR###: Out data wrong, expected " & to_string(0) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+      dat_sti <= std_logic_vector(to_unsigned(i + 1 + prev_value_v, Width_g));
+      --*** due to RAM latency of 2 clock cycle ***
+      if i < 2 then
+        StdlvCompareInt(from_uslv(hold_v) + i, dat_obs, "Out data wrong", false);
+      elsif i >= 2 and i < diff_v + 2 then
+        StdlvCompareStdlv(hold2_v, dat_obs, "Out data wrong");
       else
-        assert unsigned(dat_obs) = i - from_uslv(del_sti) report "###ERROR###: Out data wrong, expected " & to_string(i-from_uslv(del_sti)) & ", got " & to_string(from_uslv(dat_obs)) severity error;
+        StdlvCompareInt(i + 1 + prev_value_v - diff_v - (prev_delay_v), dat_obs, "Out data wrong", false);
       end if;
+
       wait until rising_edge(clk_sti);
       str_sti <= '0';
       wait until rising_edge(clk_sti);
       wait until rising_edge(clk_sti);
       wait until rising_edge(clk_sti);
     end loop;
-    str_sti <= '0';
+    ---
+    str_sti <= '1';
+    --re init
+    dat_sti <= (others => '0');
+    wait for from_uslv(del_sti) * period_c;
+    wait until rising_edge(clk_sti);
+    wait until rising_edge(clk_sti);
+    wait until rising_edge(clk_sti);
+    del_sti <= to_uslv(MaxDelay_g - 1, del_sti'length);
+    str_sti <= '1';
+    dat_sti <= to_uslv(100, dat_sti'length);
+    wait for 2 * from_uslv(del_sti) * period_c;
+    wait until rising_edge(clk_sti);
+    wait until rising_edge(clk_sti);
+    wait until rising_edge(clk_sti);
 
     -- end of process !DO NOT EDIT!
     tb_run_s <= false;
