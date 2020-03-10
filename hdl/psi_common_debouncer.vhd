0017afedac62da8140b011754acdc7e94028da10
stef_b <benoit.stef@psi.ch>
2020-03-10 11:42:22 +0100

    DEVEL: psi_common_debouncer review incorporated + counter is stopped when elapsed + input vector length added via generic

--- hdl/psi_common_debouncer.vhd
+++ hdl/psi_common_debouncer.vhd
@@ -24,13 +24,14 @@ entity psi_common_debouncer is
   generic(dbnc_per_g : real       := 10.0e-3; -- filter time in sec
           freq_clk_g : real       := 100.0e6; -- clock frequency in Hz
           rst_pol_g  : std_logic  :='1';      -- polarity reset 
+          len_g      : positive   := 1;       -- vector input lenght 
           in_pol_g   : std_logic  :='0';      -- active high or low
           out_pol_g  : std_logic  :='1';      -- active high or low
           sync_g     : boolean    := true);   -- add 2 DFF input sync
   port(   clk_i      : in  std_logic;
           rst_i      : in  std_logic;
-          inp_i      : in  std_logic;
-          out_o      : out std_logic
+          inp_i      : in  std_logic_vector(len_g-1 downto 0);
+          out_o      : out std_logic_vector(len_g-1 downto 0)
     );
 end entity;
 
@@ -42,20 +43,19 @@ architecture RTL of psi_common_debouncer is
   
   type two_process_t is record
     counter : unsigned(log2ceil(count_max_c)-1 downto 0);
-    inp_dff : std_logic; 
-    output  : std_logic;
+    inp_dff : std_logic_vector(len_g-1 downto 0); 
+    output  : std_logic_vector(len_g-1 downto 0);
   end record; 
    
-  constant two_process_c  : two_process_t :=((others=>'0'),not in_pol_g, not out_pol_g);
-  signal r, r_next        : two_process_t := two_process_c;
-  
-  signal inp_sync_s       : std_logic;
+  constant rst_two_process_c  : two_process_t :=((others=>'0'), (others=>not in_pol_g), (others=>not out_pol_g));
+  signal r, r_next            : two_process_t := rst_two_process_c;  
+  signal inp_sync_s           : std_logic_vector(len_g-1 downto 0);
 
 begin
 
   --*** double stage synchronizer ***  
   gene_sync : if sync_g generate
-    signal dff_s : std_logic;
+    signal dff_s : std_logic_vector(len_g-1 downto 0);
     begin
       process(clk_i)
       begin
@@ -83,7 +83,9 @@ begin
     if (r.inp_dff /= inp_sync_s) then
       v.counter := to_unsigned(count_max_c,v.counter'length);
     else
-      v.counter := r.counter-1;
+      if r.counter /= 0 then
+        v.counter := r.counter-1;
+      end if;
     end if;
     
     --*** check pol I/O and counter done => assign output ***
@@ -109,9 +111,7 @@ begin
     if rising_edge(clk_i) then
       r <= r_next; 
       if rst_i = rst_pol_g then
-        r.counter <= (others=>'0');
-        r.inp_dff <= not in_pol_g;
-        r.output  <= not out_pol_g;
+        r <= rst_two_process_c;
       end if;      
     end if;
   end process;  
--- testbench/psi_common_debouncer_tb/psi_common_debouncer_tb.vhd
+++ testbench/psi_common_debouncer_tb/psi_common_debouncer_tb.vhd
@@ -22,6 +22,7 @@ entity psi_common_debouncer_tb is
   generic(dbnc_per_g : real       := 20.0e-6; -- filter time in sec
           freq_clk_g : real       := 100.0e6; -- clock frequency in Hz
           rst_pol_g  : std_logic  :='1';      -- polarity reset 
+          len_g      : positive   := 10;       -- vecto input length
           sync_g     : boolean    := true);   -- add 2 DFF input sync
 end entity;
 
@@ -33,8 +34,8 @@ architecture tb of psi_common_debouncer_tb is
   signal clk_sti    : std_logic := '1';
   signal rst_sti    : std_logic := '1';
   signal tb_run_s   : boolean   := true;
-  signal inp_sti    : std_logic := not in_pol_c;
-  signal out_obs    : std_logic;
+  signal inp_sti    : std_logic_vector(len_g-1 downto 0):= (others=> not in_pol_c);
+  signal out_obs    : std_logic_vector(len_g-1 downto 0);
   
 begin
   
@@ -59,6 +60,7 @@ begin
     generic map(dbnc_per_g => dbnc_per_g,
                 freq_clk_g => freq_clk_g,
                 rst_pol_g  => rst_pol_g,
+                len_g      => len_g,
                 in_pol_g   => in_pol_c,
                 out_pol_g  => out_pol_c,
                 sync_g     => sync_g)
@@ -69,6 +71,7 @@ begin
     
   --*** stim process ***
   proc_stim : process
+    constant test : std_logic_vector(len_g-1 downto 0):=(others=>(out_pol_c));
   begin
     ------------------------------------------------------------
     print(" *******************************************  ");
@@ -82,15 +85,15 @@ begin
     rst_sti <= '0';
     
     for i in 0 to 3 loop
-      inp_sti <= not in_pol_c;
+      inp_sti <= (others=> not in_pol_c);
       wait for dbnc_per_g/2.0*sec;
       inp_sti <= not inp_sti;
       wait for dbnc_per_g/2.0*sec;
-       StdlCompare(not out_pol_c, out_obs, "debounced output not preoperly filtered");
+       StdlvCompareStdlv((not test), out_obs, "debounced output not preoperly filtered");
     end loop;
     
     wait for 5.0*dbnc_per_g*sec;
-    StdlCompare(out_pol_c, out_obs, "debounced didn't occur");
+    StdlvCompareStdlv(test, out_obs, "debounced didn't occur");
     
     for i in 0 to 3 loop
       inp_sti <= not inp_sti;
